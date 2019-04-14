a3c43e35383aa4c15d233e2e39d1cee26b66c926
Mallory Coleman <mallorycoleman@Mallorys-MacBook-Air.local>
2019-04-14 08:59:26 -0400

    tried adding js gallery and video

--- js/main.js
+++ js/main.js
@@ -3,6 +3,30 @@
 	
 	console.log("fired!!!");
 
+
+	const sigils	= document.querySelectorAll('.sigilContainer'),
+	lightbox 		= document.querySelector('.lightbox'),
+	video 			= document.querySelector('video'),
+	closeButton		= document.querySelector('.lightbox-close'),
+
+	function playVideo(){
+		lightbox.classList.add('lightbox-on');
+		video.load();
+		video.play();
+	}
+
+	video.src = `video/traa_video${targetVidoe}..mov`; 
+
+	setTimeout(playVideo, 1000);
+
+	function closeLightbox() {
+		lightbox.classList.remove('lightbox-on');
+		video.currentTime = 0;
+		video.pause();
+}
+
+
+
 	var button = document.querySelector("#button");
 	var burgerCon = document.querySelector("#burgerCon");
 
@@ -24,6 +48,10 @@
 	button.addEventListener("click", hamburgerMenu, false);
 
 	window.addEventListener("resize", closeMenu);
+
+	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+	video.addEventListener('ended', closeLightbox);
+	closeButton.addEventListener('click', closeLightbox);
 
 		
 })();
